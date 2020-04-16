3582dc9cf754c4213e993ebaeadf9bf5c860e537
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2020-04-16 10:29:16 +0200

    allow single file builds

--- site/tutorials/builder.sh
+++ site/tutorials/builder.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-for file in ./*.md
-do
-  # do not touch generated files
-  if [[ $file != *"_gen.md"* ]] && [[ $file != *"tutorial-template.md"* ]] && [[ $file != *"README.md"* ]]; then 
-    ./markymark "$file"
-    # export the generated files
-    claat export ${file/.md/_gen.md}
-  fi
-done
+if [[ $1 != "" ]]; then
+  ./markymark $1
+  claat export ${1/.md/_gen.md}
+else
 
+  for file in ./*.md
+  do
+    # do not touch generated files
+    if [[ $file != *"_gen.md"* ]] && [[ $file != *"tutorial-template.md"* ]] && [[ $file != *"README.md"* ]]; then 
+      ./markymark "$file"
+      # export the generated files
+      claat export ${file/.md/_gen.md}
+    fi
+  done
+
+fi
 # now serve the content to check locally
 # claat serve
